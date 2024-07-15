2046388c9edc024204e04ff32e33c61c89e5b8f1
zhangyitong <15269899725@163.com>
2024-07-16 01:54:40 +0800

    创建vue项目

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data() {
+      return {
+        message: 'test'
+      }
+    }
+  }
+</script>
