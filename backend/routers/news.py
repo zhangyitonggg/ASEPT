c7e7fff18e56e458e6f57eb637550f558f1636cb
吴旭飞 <22373304@buaa.edu.cn>
2024-07-18 20:03:07 +0800

    add comments

--- backend/routers/news.py
+++ backend/routers/news.py
@@ -15,4 +15,30 @@ async def get_announcements(
     max_announcements: int = 3,
     db: pymysql.connections.Connection = Depends(database.connect)
 ):
+    '''
+    max_announcements: int 类型，最多获取的公告数量，默认为 3
+
+    返回值：json 格式的公告列表。is_active 为表示公告是否有效。
+
+    ```
+    {
+        "announcements": [
+            {
+                "title": "公告标题",
+                "content": "公告内容",
+                "update_at": "2021-01-01 00:00:00",
+                "is_active": 1
+                "author": "zyt"
+            },
+            {
+                "title": "公告标题",
+                "content": "公告内容",
+                "update_at": "2021-01-01 00:00:00",
+                "is_active": 1
+                "author": "zyt"
+            }
+        ]
+    }
+    ```
+    '''
     return database.get_announcements(db, max_announcements)
