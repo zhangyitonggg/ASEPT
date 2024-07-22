f06563035ff604047764b088fdeec90e97914ae3
zhangyitong <15269899725@163.com>
2024-07-22 20:21:24 +0800

    又写出bug来了www

--- frontend/src/components/Feedback.vue
+++ frontend/src/components/Feedback.vue
@@ -4,28 +4,58 @@
         <v-text-field
           v-model="name"
           :rules="[rules.required]"
-          label="Name"
+          label="您的称呼*"
           required
         ></v-text-field>
   
         <v-text-field
           v-model="email"
           :rules="[rules.required, rules.email]"
-          label="Email"
-          required
+          label="您的邮箱地址*"
         ></v-text-field>
   
         <v-textarea
-          v-model="message"
-          :rules="[rules.required]"
-          label="Message"
-          required
+          v-model="advice"
+          label="您对我们的产品有什么建议吗？(可选)"
         ></v-textarea>
+  
+        <v-textarea
+          v-model="complaint"
+          label="投诉(可选)"
+        ></v-textarea>
+  
+        <span>
+          请给我们的产品打分
+        </span>
+        <v-rating
+          v-model="rating"
+          background-color="yellow"
+          color="yellow darken-3"
+          large
+        ></v-rating>
   
         <v-btn :disabled="!valid" @click="submit">
           Submit
         </v-btn>
       </v-form>
+  
+      <!-- 感谢图片对话框 -->
+      <v-dialog v-model="showThankYouDialog" persistent max-width="400px">
+        <v-card>
+          <v-card-title class="headline">感谢您的反馈！</v-card-title>
+          <v-card-text>
+            <v-img
+              src="../assets/thanks.png"
+              alt="Thank you"
+              max-width="100%"
+            ></v-img>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" @click="closeDialog">关闭</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </v-container>
   </template>
   
@@ -36,7 +66,10 @@
         valid: false,
         name: '',
         email: '',
-        message: '',
+        advice: '',
+        complaint: '',
+        rating: null,
+        showThankYouDialog: false, // 控制感谢图片对话框的显示
         rules: {
           required: value => !!value || 'Required.',
           email: value => {
@@ -49,16 +82,12 @@
     methods: {
       submit() {
         if (this.$refs.form.validate()) {
-          // 假设 sendFeedback 是一个发送邮件的函数
-          sendFeedback(this.name, this.email, this.message)
-            .then(() => {
-              this.$emit('success', 'Your feedback has been sent!')
-            })
-            .catch(err => {
-              this.$emit('error', 'Failed to send feedback.')
-              console.error(err)
-            })
+          console.log(this.name, this.email, this.advice, this.complaint, this.rating)
+          this.showThankYouDialog = true // 提交后显示感谢图片对话框
         }
+      },
+      closeDialog() {
+        this.showThankYouDialog = false
       }
     }
   }
--- frontend/src/views/PersonalCenter.vue
+++ frontend/src/views/PersonalCenter.vue
@@ -1,101 +1,70 @@
 <template>
-  <v-container>
-    <v-form v-model="valid" ref="form">
-      <v-text-field
-        v-model="name"
-        :rules="[rules.required]"
-        label="您的称呼*"
-        required
-      ></v-text-field>
-
-      <v-text-field
-        v-model="email"
-        :rules="[rules.required, rules.email]"
-        label="您的邮箱地址*"
-      ></v-text-field>
-
-      <v-textarea
-        v-model="advice"
-        label="您对我们的产品有什么建议吗？(可选)"
-      ></v-textarea>
-
-      <v-textarea
-        v-model="complaint"
-        label="投诉(可选)"
-      ></v-textarea>
-
-      <span>
-        请给我们的产品打分
-      </span>
-      <v-rating
-        v-model="rating"
-        background-color="yellow"
-        color="yellow darken-3"
-        large
-      ></v-rating>
-
-      <v-btn :disabled="!valid" @click="submit">
-        Submit
+  <v-container
+    class="spacing-playground pa-16"
+    fluid
+  > <component :is="this.activeBtn" />
+    <v-bottom-navigation
+      app
+      fixed
+      color="primary"
+      v-model="activeBtn"
+      @change="handleNavigateClick"
+    >
+      <v-btn value="myinfo">
+        <span>个人信息</span>
+        <v-icon>mdi-account-settings</v-icon>
       </v-btn>
-    </v-form>
-
-    <!-- 感谢图片对话框 -->
-    <v-dialog v-model="showThankYouDialog" persistent max-width="400px">
-      <v-card>
-        <v-card-title class="headline">感谢您的反馈！</v-card-title>
-        <v-card-text>
-          <v-img
-            src="../assets/thanks.png"
-            alt="Thank you"
-            max-width="100%"
-          ></v-img>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="primary" @click="closeDialog">关闭</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+      <v-btn value="fontSetting">
+        <span>页面设置</span>
+        <v-icon>mdi-cog</v-icon>
+      </v-btn>
+      <v-btn value="feedback">
+        <span>反馈</span>
+        <v-icon>mdi-invoice</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
   </v-container>
 </template>
 
 <script>
+import myinfo from '../components/MyInfo.vue'
+import feedback from '../components/Feedback.vue'
+
 export default {
+  name: 'HomeView',
+  components: {
+    myinfo,
+    feedback,
+  },
   data() {
     return {
-      valid: false,
-      name: '',
-      email: '',
-      advice: '',
-      complaint: '',
-      rating: null,
-      showThankYouDialog: false, // 控制感谢图片对话框的显示
-      rules: {
-        required: value => !!value || 'Required.',
-        email: value => {
-          const pattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
-          return pattern.test(value) || 'Invalid e-mail.'
-        }
-      }
+      activeBtn: 'myinfo',
+      loading: true,
     }
   },
+  computed: {
+  },
   methods: {
-    submit() {
-      if (this.$refs.form.validate()) {
-        console.log(this.name, this.email, this.advice, this.complaint, this.rating)
-        this.showThankYouDialog = true // 提交后显示感谢图片对话框
-      }
+    handleNavigateClick(newValue) {
+      this.$store.commit('setAppTitle', (() => {
+        switch (newValue) {
+          case 'fontSetting':
+            return '页面设置';
+          case 'feedback':
+            return '反馈';
+          default:
+            return '个人信息';
+        }
+      })());
     },
-    closeDialog() {
-      this.showThankYouDialog = false
-    }
+  },
+  watch: {
   }
 }
 </script>
 
-<style scoped>
-.v-container {
-  max-width: 600px;
-  margin: 0 auto;
+<style>
+.v-bottom-navigation {
+  bottom: 0;
 }
 </style>
