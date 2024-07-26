da7164014d1c84b153a1b90da44ad0496b9a474e
wxm <22373007@buaa.edu.cn>
2024-07-26 21:32:57 +0800

    修改按钮位置

--- frontend/src/components/exercises/Problem.vue
+++ frontend/src/components/exercises/Problem.vue
@@ -1,215 +1,3 @@
-<!-- <template>
-  <div>
-    <v-container fluid class="d-flex justify-center align-center" v-if="loading">
-      <v-progress-circular
-        indeterminate
-        color="primary"
-        size="64"
-      ></v-progress-circular>
-    </v-container>
-    <v-container fluid v-else>
-      <v-layout>
-        <v-spacer/>
-        <v-flex xs24>
-          <searchbar v-model="search" searchBtnText='搜索题目'/>
-        </v-flex>
-      </v-layout>
-      <v-col>
-        <v-list three-line>
-          <template v-for="(item, index) in currentPageItems">
-           
-            <v-divider
-              v-if="item.divider"
-              :key="index"
-              :inset="item.inset"
-            ></v-divider>
-            <v-list-item
-              v-else-if="item.title"
-              :key="item.pid"
-            >
-              <v-list-item-avatar>
-                <v-icon> mdi-help-circle-outline</v-icon>
-              </v-list-item-avatar>
-              <v-list-item-content>
-                <v-list-item-title>
-                  <h4>
-                    {{ item.title }}
-                  </h4>
-                </v-list-item-title>
-                <v-list-item-subtitle>
-                  Tag: {{ item.tag }}
-                </v-list-item-subtitle>
-              </v-list-item-content>
-              <v-list-item-action>
-                <v-btn
-                  color="primary"
-                  @click="solveProblem(item)"
-                > 去做题 </v-btn>
-              </v-list-item-action>
-
-            </v-list-item>
-          </template>
-          <v-pagination v-model="currentPage" :length="numberOfPages"></v-pagination>
-        </v-list>
-      </v-col>
-  </v-container>
-  </div>
-</template>
-
-<script>
-import searchbar from '../SearchBar.vue'
-
-export default {
-  data () {
-    return {
-      itemsPerPageArray: [4, 8, 12],
-      search: '',
-      filter: {},
-      sortDesc: false,
-      page: 1,
-      loading: true,
-      itemsPerPage: 13,
-      currentPage: 1,
-      sortBy: 'name',
-      keys: [
-        'Name',
-        'Tag',
-      ],
-      items: [
-        { header: '题目列表' },
-        {
-          name: 'Problem1',
-          tag: 'Descrlines.',
-          gid: 'xxx1',
-          locked: true,
-          content: '66666666',
-        },
-        { divider: true, inset: true },
-        {
-          name: 'Group2',
-          //founder: 'User2',
-          tag: 'Descrlines.',
-          gid: 'xxx2',
-          locked: true,
-        },
-        { divider: true, inset: true },
-        {
-          name: 'Group3',
-          founder: 'User3',
-          description: 'Descrlines.',
-          gid: 'xxx3',
-          locked: false,
-        },
-        { divider: true, inset: true },
-        
-      ],
-    }
-  },
-  created() {
-    this.fetchProblems();
-  },
-  computed: {
-    numberOfPages () {
-      return Math.ceil(this.items.length / this.itemsPerPage)
-    },
-    filteredKeys () {
-      return this.keys.filter(key => key !== 'Name')
-    },
-    filteredItems() {
-      const filtered = this.items.filter(item =>
-        item.title && item.title.toLowerCase().includes(this.search.toLowerCase())
-      );
-      return filtered;
-    },
-    currentPageItems() {
-      const startIndex = (this.currentPage - 1) * this.itemsPerPage;
-      const endIndex = startIndex + this.itemsPerPage;
-      return this.filteredItems.slice(startIndex, endIndex);
-    },
-  },
-  methods: {
-     fetchProblems() {
-        this.$store
-        .dispatch('getMyProblem')
-        .then(res => {
-         
-          this.items.splice(0, this.items.length, { header: '我创建的题目' }, ...res.problems); // 清空当前数组并插入新数据
-          console.log("我创建的：",this.items);
-        })
-        .catch(error => {
-             this.$store.commit("setAlert", {
-             type: "error",
-            message: error,
-          });
-          }).finally(() => {
-          this.loading = false;
-        })
-        .finally(() => {
-          this.loading = false;
-        });
-    },
-    nextPage () {
-      if (this.page + 1 <= this.numberOfPages) this.page += 1
-    },
-    formerPage () {
-      if (this.page - 1 >= 1) this.page -= 1
-    },
-    updateItemsPerPage (number) {
-      this.itemsPerPage = number
-    },
-    solveProblem(item){
-      console.log('pid:',item.pid );
-      this.$router.push({path:'solve/' + item.pid, append:true});
-    },
-  },
-  components: {
-    searchbar
-  }
-}
-</script>
-
-<style scoped>
-.scrollable-container {
-  position: relative;
-  top: -10%;
-  height: 100%;
-  overflow: auto;
-}
-
-.header-title {
-  font-size: 18px;
-  font-weight: 500;
-  color: #ffffff;
-  margin-right: 30%;
-}
-
-.pagination-info {
-  font-size: 16px;
-  font-weight: bold;
-  margin-left: 2%; /* Adjust as needed to move it slightly to the right */
-}
-
-.item-card {
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-  position: relative;
-}
-
-.description-text {
-  display: block;
-  white-space: normal; /* Allow text to wrap onto multiple lines */
-}
-
-.apply-button {
-  font-size: 14px;
-  position: absolute;
-  color: white;
-  font-weight:900;
-  top: 15px;
-  right: 20px;
-}
-</style> -->
 <template>
   <div>
     <v-container fluid class="d-flex justify-center align-center" v-if="loading">
@@ -220,7 +8,11 @@ export default {
       ></v-progress-circular>
     </v-container>
     <v-container fluid v-else>
+      
+      
       <v-layout>
+        <!-- 查找题目按钮 -->
+        <v-btn color="primary" @click="showSearchDialog = true">查找题目</v-btn>
         <v-spacer/>
         <v-flex xs24>
           <searchbar v-model="search" searchBtnText='搜索题目'/>
@@ -262,10 +54,7 @@ export default {
           <v-pagination v-model="currentPage" :length="numberOfPages"></v-pagination>
         </v-list>
       </v-col>
-      <!-- 查找题目按钮 -->
-      <v-layout justify-center>
-        <v-btn color="primary" @click="showSearchDialog = true">查找题目</v-btn>
-      </v-layout>
+      
       <!-- 查找题目弹窗 -->
       <v-dialog v-model="showSearchDialog" max-width="800px">
         <v-card>
