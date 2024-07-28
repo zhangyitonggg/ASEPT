8fb8f66f2d9446ee849b3c4ec4c3db0d256feea4
zhangyitong <15269899725@163.com>
2024-07-28 21:00:52 +0800

    fix bug

--- backend/utils/database.py
+++ backend/utils/database.py
@@ -1159,6 +1159,7 @@ def get_problem_recommend(db, user: User):
             "is_public": problem[8],
             "tags": _tags_,
         })
+    ret = random.sample(ret, min(7, len(ret)))
     return {"problems": ret}
     
 
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -190,6 +190,7 @@ export default new Vuex.Store({
             resolve(response.data);
           })
           .catch(error => {
+            console.log(error);
             reject(error);
           })
       })
